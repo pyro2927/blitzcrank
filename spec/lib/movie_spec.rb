e5038357dcd5150fc35a87d5b90ce059f49c255e
pyro2927 <joseph@pintozzi.com>
2014-09-23 14:35:01 -0500

    Adding some local_path tests to movies

--- spec/lib/movie_spec.rb
+++ spec/lib/movie_spec.rb
@@ -6,10 +6,20 @@ module Blitzcrank
                              'Army.Of.Darkness.1992.Bluray.1080p.DTSMA.x264.mkv']}
     let(:movies) { movie_file_names.map {|f| Movie.new f } }
 
+    before(:all) do
+      Blitzcrank.config.update(base_movie_dir: 'Movies/')
+    end
+
     it "correctly detects movies" do
       movies.each do |video|
         expect(video.is_movie?).to eq(true)
         expect(video.is_tv_show?).to eq(false)
+      end
+    end
+
+    it "correctly gets the local path for a movie" do
+      movies.each_with_index do |video, i|
+        expect(video.local_path).to eql("Movies/#{movie_file_names[i]}")
       end
     end
 
