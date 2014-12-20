7034c90028d004d198494d9551f422ba5c646aaa
pyro2927 <joseph@pintozzi.com>
2014-12-19 23:55:26 -0600

    Escaping ampersands

--- lib/blitzcrank/rsync.rb
+++ lib/blitzcrank/rsync.rb
@@ -1,8 +1,12 @@
 module Blitzcrank
   class Rsync < Copy
     def self.sync(video)
-      command = "rsync -avz #{ '--bwlimit=' + Blitzcrank.config.bwlimit if Blitzcrank.config.respond_to?(:bwlimit) } --progress --rsh='ssh' \"#{Blitzcrank.config.remote_user}@#{Blitzcrank.config.remote_host}:#{Blitzcrank.config.remote_base_dir}#{video.remote_path.gsub(' ', '\\ ').gsub("'", %q(\\\'))}\" \"#{video.local_path}\""
+      command = "rsync -avz #{ '--bwlimit=' + Blitzcrank.config.bwlimit if Blitzcrank.config.respond_to?(:bwlimit) } --progress --rsh='ssh' \"#{Blitzcrank.config.remote_user}@#{Blitzcrank.config.remote_host}:#{Blitzcrank.config.remote_base_dir}#{self.escaped_string(video.remote_path)}\" \"#{video.local_path}\""
       system(command)
+    end
+
+    def self.escaped_string(string)
+      string.gsub(' ', '\\ ').gsub("'", %q(\\\')).gsub("&"){'\&'}
     end
   end
 end
