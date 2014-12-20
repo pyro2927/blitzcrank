c9167a3acaed3def6a01c39303e4fa3ee34d7622
pyro2927 <joseph@pintozzi.com>
2014-12-19 23:21:43 -0600

    Fixing single quote escaping issues

--- lib/blitzcrank/rsync.rb
+++ lib/blitzcrank/rsync.rb
@@ -1,7 +1,8 @@
 module Blitzcrank
   class Rsync < Copy
     def self.sync(video)
-      system("rsync -avz #{ '--bwlimit=' + Blitzcrank.config.bwlimit if Blitzcrank.config.respond_to?(:bwlimit) } --progress --rsh='ssh' \"#{Blitzcrank.config.remote_user}@#{Blitzcrank.config.remote_host}:#{Blitzcrank.config.remote_base_dir}#{video.remote_path.gsub(' ', '\\ ')}\" \"#{video.local_path}\"")
+      command = "rsync -avz #{ '--bwlimit=' + Blitzcrank.config.bwlimit if Blitzcrank.config.respond_to?(:bwlimit) } --progress --rsh='ssh' \"#{Blitzcrank.config.remote_user}@#{Blitzcrank.config.remote_host}:#{Blitzcrank.config.remote_base_dir}#{video.remote_path.gsub(' ', '\\ ').gsub("'", %q(\\\'))}\" \"#{video.local_path}\""
+      system(command)
     end
   end
 end
