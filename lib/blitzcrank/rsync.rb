9456c590f72d2d6fb151b096cd48231b5c55322c
pyro2927 <joseph@pintozzi.com>
2014-12-19 21:37:19 -0600

    Using rsync gem

--- lib/blitzcrank/rsync.rb
+++ lib/blitzcrank/rsync.rb
@@ -1,7 +1,16 @@
+require "rsync"
 module Blitzcrank
   class Rsync < Copy
     def self.sync(video)
-      system("rsync -avz #{ '--bwlimit=' + Blitzcrank.config.bwlimit if Blitzcrank.config.respond_to?(:bwlimit) } --progress --rsh='ssh' \"#{Blitzcrank.config.remote_user}@#{Blitzcrank.config.remote_host}:#{Blitzcrank.config.remote_base_dir}#{video.remote_path.gsub(' ', '\\ ')}\" \"#{video.local_path}\"")
+      options = ["--rsh='ssh'"]
+      options << '--bwlimit=' + Blitzcrank.config.bwlimit if Blitzcrank.config.respond_to?(:bwlimit)
+      Rsync.run("#{Blitzcrank.config.remote_user}@#{Blitzcrank.config.remote_host}:#{Blitzcrank.config.remote_base_dir}#{video.remote_path}", video.local_path, options) do |result|
+        if result.success?
+          puts "File successfully transfered"
+        else
+          puts result.error
+        end
+      end
     end
   end
 end
